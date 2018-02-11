d9088c788939e859a4c44eff2022504b5d16af46
zhanghonqqiao <991034150@qq.com>
2018-02-11 18:20:20 +0800

    add left nav

--- src/components/layout/siderMenus.js
+++ src/components/layout/siderMenus.js
@@ -3,7 +3,7 @@
  * @Date: 2018-02-11 15:09:38 
  * @Description: 左边菜单导航
  * @Last Modified by: zhanghongqiao@hiynn.com
- * @Last Modified time: 2018-02-11 17:59:52
+ * @Last Modified time: 2018-02-11 18:19:21
  */
 
 import React from 'react'
@@ -28,15 +28,14 @@ function MenuItem(props) {
 }
 
 function MenuItems(props) {
-  const itemLists = props.charts.map((d, i) =>
+   return props.charts.map((d, i) =>
     <SubMenu
       key={d.key}
-      title={<span><Icon type={d.key} /><span>{d.typeName}</span></span>}
+      title={<span><Icon type={d.key} /><span>{d.typeName}</span></span>}  
     >
     </SubMenu>  
   )
-  console.log(itemLists)
-  return  itemLists
+  
 }
 
 const SiderMenus = ({match}) => (
@@ -53,8 +52,34 @@ const SiderMenus = ({match}) => (
         <span>首页</span>
       </Link>
     </Menu.Item>
+    {/* <MenuItems charts={charts} /> */}
     
-    <MenuItems charts={charts} />
+    {/* 表格 */}
+    <SubMenu
+        key="sub1"
+        title={<span><Icon type="switcher" /><span>表格</span></span>}
+      >
+        <Menu.Item key="/tables"><Link to="/tables">简单表格</Link></Menu.Item>
+        <Menu.Item key="/asyc-tables"><Link to="/asyc-tables">简单异步表格</Link></Menu.Item>
+      </SubMenu>
+      {/* 个人中心 */}
+      <SubMenu
+        key="sub2"
+        title={<span><Icon type="user" /><span>个人中心</span></span>}
+      >
+        <Menu.Item key="/profile"><Link to="/profile">个人信息</Link></Menu.Item>
+      </SubMenu>
+      {/* 数学图形 */}
+      <SubMenu
+        key="sub3"
+        title={<span><Icon type="dot-chart" /><span>数学图形</span></span>}
+      >
+        <Menu.Item key="/simple-math-chart"><Link to="/simple-math-chart">常用函数图像</Link></Menu.Item>
+        <Menu.Item key="/archimedean-spiral-line"><Link to="/archimedean-spiral-line">阿基米德螺线</Link></Menu.Item>
+        <Menu.Item key="/equiangular-spiral-line"><Link to="/equiangular-spiral-line">等角螺线</Link></Menu.Item>
+        <Menu.Item key="/exponential-spiral-line"><Link to="/exponential-spiral-line">指数函数图像-极坐标</Link></Menu.Item>
+        <Menu.Item key="/logarithmic-spiral-line"><Link to="/logarithmic-spiral-line">对数函数图像-极坐标</Link></Menu.Item>
+      </SubMenu>
   </Menu>    
   </div>
 )
